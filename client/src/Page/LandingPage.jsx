60345008706da3bd6b377badb0f771b284a153d9
Amrit Kumar <amrit24680gg@gmail.com>
2025-07-14 17:12:09 +0530

    fix import logo issue

--- client/src/Page/LandingPage.jsx
+++ client/src/Page/LandingPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import logo from "../assets/logo.png";
+import logo from "../assets/Logo.png";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { useNavigate } from "react-router";
 import axios from "axios";
